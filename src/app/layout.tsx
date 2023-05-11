caa2ec95d9d345c7fb2c333cca8aa401e7f5ae24
Martin Porjanda <m.porjanda@gmail.com>
2023-05-11 12:09:02 +0200

    fix issue with metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,11 @@ import "antd/dist/reset.css";
 import "@/global.scss";
 
 import React from "react";
-import {Metadata} from "next";
 import Container from "@/components/container/container";
 import NavigationLogo from "@/components/navigation/navigationLogo";
 import {ConfigProvider, Layout} from "antd";
 
 const {Header, Content} = Layout;
-
-export const metadata: Metadata = {
-    title: 'Martin Porjanda - Test task ',
-    description: 'Test task - single web application for rendering data in charts',
-}
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
